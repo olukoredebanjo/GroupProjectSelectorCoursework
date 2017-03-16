adc9ec9c64691cc3c6198a561824a3d9d2ff06cd
Adubiobi oluwakorede <1614132@rgu.ac.uk>
2017-03-16 16:10:28 +0000

    initial

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,68 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Adubiobi oluwakorede
- * Date: 16-Mar-17
- * Time: 02:50 PM
- */
+
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value) {
+
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+
+        continue;
+
+    }
+
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+
+}
+
+
+
+$db = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+
+
+/*
+
+define('DB_SERVER', $connectstr_dbhost);
+
+define('DB_USERNAME', $connectstr_dbname);
+
+define('DB_PASSWORD', $connectstr_dbusername);
+
+define('DB_DATABASE', $connectstr_dbpassword);
+
+*/
+
+
+
+if (!$db) {
+
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+
+    exit;
+
+}
+
+/*
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+
+echo "Host information: " . mysqli_get_host_info($db) . PHP_EOL;
+
+mysqli_close($db);
+
+*/
+
+?>
