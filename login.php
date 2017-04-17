b55064e656a224596042ca8898957fe1a838d0d3
Adubiobi oluwakorede <1614132@rgu.ac.uk>
2017-04-17 14:11:18 +0100

    initial

--- login.php
+++ login.php
@@ -7,22 +7,22 @@ if($username=($_POST["empty"]) ||$password=($_POST["empty"]))
 {
     echo "Both fields are required. ";
 }
-else
-    $username=$_POST["username"];
-    $password=$_POST["password"];
+else {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
+    $sql = "select uid from users where username=$username and password=$password;";
+    $result = mysqli_query($db, $sql);
 
-    {
+    if (mysqli_num_rows($result) == 1) {
         header("location: home.html"); // Redirecting To another Page
-    }else
-
-    {
+    } else {
         echo "Incorrect username or password. ";
     }
+}
 
+$result =closelog();
 
-
-$result->close();
 $db->close();
 
 ?>
