b8814e6617ee7b5f84ba7552db053ead9afda0d5
Adubiobi oluwakorede <1614132@rgu.ac.uk>
2017-04-17 04:00:32 +0100

    initial

--- login.php
+++ login.php
@@ -8,9 +8,8 @@ if(empty($_POST[ "username"]) || empty($_POST[ "password"]))
     echo "Both fields are required. ";
 }else
 {
-
-    $username=$_POST['username1'];
-    $password=$_POST['password1'];
+    $username=$_POST["username"];
+    $password=$_POST["password"];
 
     $sql = "SELECT uid FROM users WHERE username='$username' and password='$password'; ";
     $result = mysqli_query($db,$sql);
