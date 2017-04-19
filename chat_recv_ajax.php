e28e55136fdbb1a92a004e6d656a956bc0368a8a
olukoredebanjo <1614132@rgu.ac.uk>
2017-04-19 17:58:44 +0100

    Update chat_recv_ajax.php

--- chat_recv_ajax.php
+++ chat_recv_ajax.php
@@ -1,17 +1,18 @@
 <?php
 
-     require_once('db.php');
+     require_once('dbconfig.php');
 
-     db_connect();
+    
      $pic="";
 	 $pic='<img src="user-icon.jpg" width="20" height="20"/>';
       
      $sql = "SELECT *, date_format(chatdate,'%d-%m-%Y %r') as cdt from chat order by ID desc limit 200";
      $sql = "SELECT * FROM (" . $sql . ") as ch order by ID";
-     $result = mysql_query($sql) or die('Query failed: ' . mysql_error());
+     $result = mysqli_query($conn, $sql);
+	 
      // Update Row Information
      $msg="<table border='0' style='font-size: 10pt; color: blue; font-family: verdana, arial;'>";
-     while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
+     while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC))
      {
            $msg = $msg . "<tr><td>" . $pic . "&nbsp;</td>" .
                 "<td>" . $line["USERNAME"] . ":&nbsp;</td>" .
