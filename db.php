77cb492e6fe0f438a2034d95052ef2d9e5bfd142
olukoredebanjo <1614132@rgu.ac.uk>
2017-04-19 17:29:05 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
-
+include "dbconnect.php";
 
 function db_connect()
 {
 
   date_default_timezone_set("Asia/Calcutta");
 
-  $link = mysql_connect("localhost", "root", "")
+  $link = mysql_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbname)
             or die('Could not connect: ' . mysql_error());
   mysql_select_db("groupproject") or die('Could not select database');
   return true;
